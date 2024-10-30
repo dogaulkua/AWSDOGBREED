6a4ed2bdecd2a979a1b8bb6f9dd52a788a784111
dogaulkua <121691964+dogaulkua@users.noreply.github.com>
2024-10-30 19:24:56 +0300

    get_input_args.py

--- get_input_args.py
+++ get_input_args.py
@@ -1,8 +1,10 @@
+import argparse
+
 def get_input_args():
     """
     Retrieves and parses the 3 command line arguments provided by the user when
     they run the program from a terminal window. This function uses Python's 
-    argparse module to created and defined these 3 command line arguments. If 
+    argparse module to create and define these 3 command line arguments. If 
     the user fails to provide some or all of the 3 arguments, then the default 
     values are used for the missing arguments. 
     Command Line Arguments:
@@ -10,23 +12,23 @@ def get_input_args():
       2. CNN Model Architecture as --arch with default value 'vgg'
       3. Text File with Dog Names as --dogfile with default value 'dognames.txt'
     This function returns these arguments as an ArgumentParser object.
+    
     Parameters:
      None - simply using argparse module to create & store command line arguments
+     
     Returns:
-     parse_args() -data structure that stores the command line arguments object  
+     parse_args() - data structure that stores the command line arguments object  
     """
-    # Create Parse using ArgumentParser
-    
-    # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
-    
-    
-    # Replace None with parser.parse_args() parsed argument collection that 
-    # you created with this function 
-    parser = argparse.ArgumentParser()
+    # Create ArgumentParser object
+    parser = argparse.ArgumentParser(description="Process command line arguments for image classification")
+
+    # Define 3 command line arguments
     parser.add_argument('--dir', type=str, default='pet_images/', 
-                        help='path to the folder of pet images')
+                        help='Path to the folder of pet images')
     parser.add_argument('--arch', type=str, default='vgg',
                         help='CNN model architecture to use for image classification')
     parser.add_argument('--dogfile', type=str, default='dognames.txt',
-                        help='path to the text file with dog names')
+                        help='Path to the text file with dog names')
+
+    # Parse and return command line arguments
     return parser.parse_args()
